92b42a3841753e82f69aba25153890be74ec4d41
cidTomsk <61188943+cidTomsk@users.noreply.github.com>
2022-04-16 04:16:51 +0700

    Тест запрса к серверу

---
+++ tests.py
@@ -0,0 +1,27 @@
+import unittest
+import requests
+from multiprocessing import Process
+from httpd import HTTPserver
+
+
+def runserver():
+    server = HTTPserver()
+    server.run()
+
+
+class TestSuite(unittest.TestCase):
+    def setUp(self):
+        self.process = Process(target=runserver)
+        self.process.start()
+
+    def tearDown(self) -> None:
+        self.process.terminate()
+
+    def test_basic_answer(self):
+        response = requests.get('http://localhost:8000')
+        print(response)
+        print(response.text)
+
+
+if __name__ == "__main__":
+    unittest.main()
