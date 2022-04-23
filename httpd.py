9349c0c480c81e7eb879433fcae3b00550bf5b5e
cidTomsk <61188943+cidTomsk@users.noreply.github.com>
2022-04-23 13:58:15 +0700

    начал форматировать ответ

--- httpd.py
+++ httpd.py
@@ -6,6 +6,7 @@ import time
 import random
 
 from optparse import OptionParser
+from datetime import datetime, timezone
 
 
 class WorkThread(threading.Thread):
@@ -33,7 +34,6 @@ class ThreadPoolManger:
             print("ThreadPoolManger")
             thread = WorkThread(self.work_queue)
             thread.start()
- #           thread.join()     # Возможная причина блокировки
 
     def add_work(self, func, *args):
         self.work_queue.put((func, args))
@@ -60,29 +60,50 @@ class HTTPserver:
 
     def run(self):
         client_connection, client_address = self.server_socket.accept()
-        thread_pool = ThreadPoolManger(self.workers)  #  BLOCKING??????????????
+        thread_pool = ThreadPoolManger(self.workers)
         while True:
             time.sleep(1)
             thread_pool.add_work(self.send_answer, *(client_connection, client_address))
 
     def request_processing(self, request):
+        """
+        !!!!!! Нужна поддержка регулярок
+        """
         print(f"request {request} ")
         if request.startswith("GET"):
             req_lines = request.split('\n')
             get_first_arg = req_lines[0].split()[1]
-            if get_first_arg == r"/":
-                return 'HTTP/1.0 200 OK\n\nHello World'
-            else:
-                if get_first_arg.startswith(r"/"):
-                    file_path = self.root + get_first_arg
+
+            if get_first_arg.startswith(r"/"):
+                if get_first_arg.startswith(r"/private_dir"):
+                    return 'HTTP/1.0 403 Forbidden'
+                file_path = self.root + get_first_arg
+                try:
+                    if file_path.endswith(r"/"):
+                        file_path += "index.html"
                     with open(file_path, 'r') as f:
                         return f'HTTP/1.0 200 OK\n\n{f.read()}'
-
-            return 'HTTP/1.0 200 OK\n\nHello World'
+                except FileNotFoundError:
+                    return 'HTTP/1.0 404 File Not Found'
+            else:
+                return 'HTTP/1.0 404 File Not Found'
         elif request.startswith("HEAD"):
             return 'HTTP/1.0 200 OK\n\nHello World'
         else:
             return 'HTTP/1.0 405 Method Not Allowed'
+
+    @staticmethod
+    def format_response(code, filename, file_date):
+        """
+        add headers Date, Server, Content-Length, Content-Type, Connection
+        """
+        response = f'HTTP/1.0 {code} \n'
+        response += f"Date:{datetime.now(timezone.utc)}"
+        response += "Server: DZ5\n"
+        if filename.endswith(".html"):
+            response += "Content - Type: text/html; charset=UTF-8"
+        response += "Connection: close\n"
+        return response
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -8,7 +8,7 @@ def runserver():
     server = HTTPserver(root="DOCUMENT_ROOT")
     server.run()
 
-
+# !!!! Мб нужны пау3ы между тестами
 class TestSuite(unittest.TestCase):
     def setUp(self):
         self.process = Process(target=runserver)
@@ -27,6 +27,20 @@ class TestSuite(unittest.TestCase):
         print(response)
         print(response.text)
 
+    def test_get_bad_file(self):
+        response = requests.get('http://localhost:8000/bad_file.html')
+        print(response)
+        print(response.text)
+
+    def test_get_dir_index(self):
+        response = requests.get('http://localhost:8000/dir/')
+        print(response)
+        print(f"DIR \n {response.text}")
+
+    def test_forbiden_request(self):
+        response = requests.get('http://localhost:8000/private_dir/')
+        print(response)
+        print(f"FORbidden \n {response.text}")
 
 if __name__ == "__main__":
     unittest.main()
